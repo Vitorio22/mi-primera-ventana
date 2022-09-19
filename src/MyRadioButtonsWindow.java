d9336020ab27d96ec35fc4f8373fe0158a85aef6
Victor <nicromante656@hotmail.com>
2022-09-19 12:32:20 +0200

    Actualizado radiobuttons con imagenes

--- src/MyRadioButtonsWindow.java
+++ src/MyRadioButtonsWindow.java
@@ -1,38 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyRadioButtonsWindow {
     public static void main(String[] args) {
         JFrame windowRadioButton = new JFrame();
-        windowRadioButton.setBounds(50, 50, 400, 400);
+        windowRadioButton.setBounds(50, 50, 350, 600);
         windowRadioButton.setVisible(true);
         windowRadioButton.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JRadioButton radioButton1 = new JRadioButton("Linux");
-        JRadioButton radioButton2 = new JRadioButton("Windows");
-        JRadioButton radioButton3 = new JRadioButton("Macintosh");
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(Color.gray);
         ButtonGroup buttonGroup = new ButtonGroup();
+        JRadioButton radioButton1 = new JRadioButton("Linux");
+        radioButton1.setActionCommand("Linux");
+        JRadioButton radioButton2 = new JRadioButton("Windows");
+        radioButton2.setActionCommand("Windows");
+        JRadioButton radioButton3 = new JRadioButton("Macintosh");
+        radioButton3.setActionCommand("Macintosh");
         buttonGroup.add(radioButton1);
         buttonGroup.add(radioButton2);
         buttonGroup.add(radioButton3);
+        JLabel imageLabel = new JLabel();
+        imageLabel.setIcon(new ImageIcon("Linux.png"));
         JButton button = new JButton("Aceptar");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selected = buttonGroup.getSelection().getActionCommand();
+                JOptionPane.showMessageDialog(windowRadioButton, "Has seleccionado " + selected);
 
+                imageLabel.setIcon(new ImageIcon(selected + ".png"));
 
-        JPanel mainPanel = new JPanel();
-        mainPanel.setBackground(Color.black);
+            }
+        });
 
-        JPanel primaryPanel = new JPanel();
-        primaryPanel.setBounds(50, 50, 500, 500);
-        primaryPanel.setBackground(Color.gray);
-
-
-
-
-
-        mainPanel.add(primaryPanel);
-        primaryPanel.add(radioButton1);
-        primaryPanel.add(radioButton2);
-        primaryPanel.add(radioButton3);
-        primaryPanel.add(button);
+        mainPanel.add(radioButton1);
+        mainPanel.add(radioButton2);
+        mainPanel.add(radioButton3);
+        mainPanel.add(imageLabel);
+        mainPanel.add(button);
 
         windowRadioButton.setContentPane(mainPanel);
         windowRadioButton.repaint();
