06c9ad9fd1b01bbb82a38b8de541680ee3ac5810
Victor <nicromante656@hotmail.com>
2022-09-27 12:32:21 +0200

    GridLayout, calculadora

--- src/org/iesfm/ventana/GridLayout.java
+++ src/org/iesfm/ventana/GridLayout.java
@@ -17,32 +17,41 @@ public class GridLayout {
         mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0,0));
 
         JPanel panel1 = new JPanel();
-        panel1.add(new JTextField(25));
+        panel1.setLayout(new BorderLayout(10, 10));
+        panel1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        JTextField textField = new JTextField("0", 20);
+        textField.setPreferredSize(new Dimension(80, 80));
+        textField.setLayout(new BorderLayout());
 
         JPanel panel2 = new JPanel();
-        panel2.setLayout(new java.awt.GridLayout(5, 4, 10, 10));
-        panel2.setBorder(BorderFactory.createEmptyBorder(50, 0,0,0));
-        JButton button1 = new JButton("Rtc");
-        JButton button2 = new JButton("CE");
-        JButton button3 = new JButton("CL");
-        JButton button4 = new JButton("+/-");
-        JButton button5 = new JButton("7");
-        JButton button6 = new JButton("8");
-        JButton button7 = new JButton("9");
-        JButton button8 = new JButton("/");
-        JButton button9 = new JButton("4");
-        JButton button10 = new JButton("5");
-        JButton button11 = new JButton("6");
-        JButton button12 = new JButton("*");
-        JButton button13 = new JButton("1");
-        JButton button14 = new JButton("2");
-        JButton button15 = new JButton("3");
-        JButton button16 = new JButton("-");
-        JButton button17 = new JButton("0");
-        JButton button18 = new JButton(".");
-        JButton button19 = new JButton("=");
-        JButton button20 = new JButton("+");
+        panel2.setLayout(new java.awt.GridLayout(4, 4, 10, 10));
+        JButton button1 = new JButton("7");
+        JButton button2 = new JButton("8");
+        JButton button3 = new JButton("9");
+        JButton button4 = new JButton("DEL");
+        button4.setBackground(Color.BLUE);
+        JButton button5 = new JButton("4");
+        JButton button6 = new JButton("5");
+        JButton button7 = new JButton("6");
+        JButton button8 = new JButton("+");
+        JButton button9 = new JButton("1");
+        JButton button10 = new JButton("2");
+        JButton button11 = new JButton("3");
+        JButton button12 = new JButton("-");
+        JButton button13 = new JButton(".");
+        JButton button14 = new JButton("0");
+        JButton button15 = new JButton("/");
+        JButton button16 = new JButton("*");
 
+        JPanel panel3 = new JPanel();
+        panel3.setLayout(new java.awt.GridLayout(1,2,10,10));
+        panel3.setPreferredSize(new Dimension(50, 50));
+        JButton button17 = new JButton("RESET");
+        button17.setBackground(Color.BLUE);
+        JButton button18 = new JButton("=");
+        button18.setBackground(Color.RED);
+
+        panel1.add(textField);
         panel2.add(button1);
         panel2.add(button2);
         panel2.add(button3);
@@ -59,13 +68,12 @@ public class GridLayout {
         panel2.add(button14);
         panel2.add(button15);
         panel2.add(button16);
-        panel2.add(button17);
-        panel2.add(button18);
-        panel2.add(button19);
-        panel2.add(button20);
+        panel3.add(button17);
+        panel3.add(button18);
 
         mainPanel.add(panel1, BorderLayout.NORTH);
         mainPanel.add(panel2, BorderLayout.CENTER);
+        mainPanel.add(panel3, BorderLayout.SOUTH);
 
         windowGridLayout.setContentPane(mainPanel);
         windowGridLayout.repaint();
