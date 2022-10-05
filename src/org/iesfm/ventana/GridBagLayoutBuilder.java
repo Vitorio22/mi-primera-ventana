042e013b69700fe5ebf41b7bc37dab9e776de325
Vitorio22 <nicromante656@hotmail.com>
2022-10-05 03:35:09 +0200

    Cambios en el builder

--- src/org/iesfm/ventana/GridBagLayoutBuilder.java
+++ src/org/iesfm/ventana/GridBagLayoutBuilder.java
@@ -8,125 +8,125 @@ public class GridBagLayoutBuilder {
     public static void main(String[] args) {
 
         JFrame windowGridBagLayout = new JFrame("GridBagLayout Builder");
-        windowGridBagLayout.setBounds(10, 10, 600, 400);
+        windowGridBagLayout.setBounds(10, 10, 522, 385);
         windowGridBagLayout.setVisible(true);
         windowGridBagLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
 
-        JLabel labelName = new JLabel("Full Name", SwingConstants.RIGHT);
+        JLabel labelFullName = new JLabel("Full Name");
         GridBagConstraints constraintsLabelName = new GridBagConstraints(
-                0, 0, 1, 1, 1, 1,
+                0, 0, 1, 1, 0, 0,
                 GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(labelFullName, constraintsLabelName);
 
-        JTextField name = new JTextField();
-        GridBagConstraints constraintsName = new GridBagConstraints(
-                1, 0, 3, 1, 1, 1,
-                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-                new Insets(1, 1, 1, 1), 0, 0);
-
-        JLabel labelStreet = new JLabel("Street");
+        JLabel street = new JLabel("Street", SwingConstants.CENTER);
         GridBagConstraints constraintsLabelStreet = new GridBagConstraints(
-                1, 1, 1, 1, 1, 1,
-                GridBagConstraints.SOUTH, GridBagConstraints.BOTH,
+                1, 1, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(street, constraintsLabelStreet);
 
-        JTextField address_street = new JTextField();
-        GridBagConstraints constraintsAdress_Street = new GridBagConstraints(
-                1, 2, 1, 1, 1, 1,
-                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(1, 1, 1, 1), 0, 0);
-
-        JLabel labelCity = new JLabel("City", SwingConstants.CENTER);
+        JLabel city = new JLabel("City", SwingConstants.CENTER);
         GridBagConstraints constraintsLabelCity = new GridBagConstraints(
-                2, 1, 1, 1, 1, 1,
-                GridBagConstraints.SOUTH, GridBagConstraints.HORIZONTAL,
+                2, 1, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(city, constraintsLabelCity);
 
-        JTextField city = new JTextField();
-        GridBagConstraints constraintsCity = new GridBagConstraints(
-                2, 2, 1, 1, 1, 1,
-                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(1, 1, 1, 1), 0, 0);
-
-        JLabel labelZipCode = new JLabel("Zip Code", SwingConstants.CENTER);
+        JLabel zipCode = new JLabel("Zip Code", SwingConstants.CENTER);
         GridBagConstraints constraintsLabelZipCode = new GridBagConstraints(
-                3, 1, 1, 1, 1, 1,
-                GridBagConstraints.SOUTH, GridBagConstraints.HORIZONTAL,
+                3, 1, 1, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(zipCode, constraintsLabelZipCode);
 
-        JTextField ZipCode = new JTextField();
-        GridBagConstraints constraintsZipCode = new GridBagConstraints(
-                3, 2, 1, 1, 1, 1,
-                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(1, 1, 1, 1), 0, 0);
-
-        JLabel labelAddress = new JLabel("Address", SwingConstants.RIGHT);
+        JLabel address = new JLabel("Address");
         GridBagConstraints constraintsLabelAddress = new GridBagConstraints(
-                0, 2, 1, 1, 1, 1,
-                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                0, 2, 3, 1, 0, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(address, constraintsLabelAddress);
 
-        JLabel labelPhone = new JLabel("Phone", SwingConstants.RIGHT);
+        JLabel phone = new JLabel("Phone");
         GridBagConstraints constraintsLabelPhone = new GridBagConstraints(
-                0, 3, 1, 1, 1, 1,
+                0, 3, 1, 1, 0, 0,
                 GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(phone, constraintsLabelPhone);
 
-        JTextField phone = new JTextField();
-        GridBagConstraints constraintsPhone = new GridBagConstraints(
-                1, 3, 1, 1, 1, 1,
-                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-                new Insets(1, 1, 1, 1), 0, 0);
-
-        JLabel labelAge = new JLabel("Age", SwingConstants.RIGHT);
+        JLabel Age = new JLabel("Age", SwingConstants.RIGHT);
         GridBagConstraints constraintsLabelAge = new GridBagConstraints(
-                2, 3, 1, 1, 1, 1,
+                2, 3, 1, 1, 0, 0,
                 GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(Age, constraintsLabelAge);
 
-        JTextField age = new JTextField();
-        GridBagConstraints constraintsAge = new GridBagConstraints(
-                3, 3, 1, 1, 1, 1,
-                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-                new Insets(1, 1, 1, 1), 0, 0);
-
-        JLabel labelDescription = new JLabel("Description", SwingConstants.RIGHT);
+        JLabel description = new JLabel("Description");
         GridBagConstraints constraintsLabelDescription = new GridBagConstraints(
-                0, 4, 1, 1, 1, 1,
+                0, 4, 1, 1, 0, 0,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(description, constraintsLabelDescription);
 
-        JTextArea description = new JTextArea();
-        GridBagConstraints constrainsDescription = new GridBagConstraints(
-                1, 4, 3, 1, 3, 3,
-                GridBagConstraints.NORTH, GridBagConstraints.BOTH,
+        JTextField textFieldFullName = new JTextField();
+        GridBagConstraints constraintsTextFieldName = new GridBagConstraints(
+                1, 0, 3, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(textFieldFullName, constraintsTextFieldName);
 
-        JButton submit = new JButton("Submit");
-        GridBagConstraints constraintsButton = new GridBagConstraints(
-                3, 5, 2, 1, 0, 0,
-                GridBagConstraints.SOUTH, GridBagConstraints.HORIZONTAL,
+        JTextField textFieldStreet = new JTextField();
+        GridBagConstraints constraintsTextFieldStreet = new GridBagConstraints(
+                1, 2, 1, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(textFieldStreet, constraintsTextFieldStreet);
 
-        mainPanel.add(labelName, constraintsLabelName);
-        mainPanel.add(name, constraintsName);
-        mainPanel.add(labelStreet, constraintsLabelStreet);
-        mainPanel.add(address_street, constraintsAdress_Street);
-        mainPanel.add(labelCity, constraintsLabelCity);
-        mainPanel.add(city, constraintsCity);
-        mainPanel.add(labelZipCode, constraintsLabelZipCode);
-        mainPanel.add(ZipCode, constraintsZipCode);
-        mainPanel.add(labelAddress, constraintsLabelAddress);
-        mainPanel.add(labelPhone, constraintsLabelPhone);
-        mainPanel.add(phone, constraintsPhone);
-        mainPanel.add(labelAge, constraintsLabelAge);
-        mainPanel.add(age, constraintsAge);
-        mainPanel.add(labelDescription, constraintsLabelDescription);
-        mainPanel.add(description, constrainsDescription);
-        mainPanel.add(submit, constraintsButton);
+        JTextField textFieldCity = new JTextField();
+        GridBagConstraints constraintsTextFieldCity = new GridBagConstraints(
+                2, 2, 1, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(textFieldCity, constraintsTextFieldCity);
+
+        JTextField textFieldZipCode = new JTextField();
+        GridBagConstraints constraintsTextFieldZipCode = new GridBagConstraints(
+                3, 2, 1, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(textFieldZipCode, constraintsTextFieldZipCode);
+
+        JTextField textFieldPhone = new JTextField();
+        GridBagConstraints constraintsTextFieldPhone = new GridBagConstraints(
+                1, 3, 1, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(textFieldPhone, constraintsTextFieldPhone);
+
+        JTextField textFieldAge = new JTextField();
+        GridBagConstraints constraintsTextFieldAge = new GridBagConstraints(
+                3, 3, 1, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(textFieldAge, constraintsTextFieldAge);
+
+        JTextArea textAreaDescription = new JTextArea();
+        GridBagConstraints constraintsTextAreaDescription = new GridBagConstraints(
+                1, 4, 3, 1, 1, 1,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(textAreaDescription, constraintsTextAreaDescription);
+
+        JButton buttonSubmit = new JButton("Submit");
+        GridBagConstraints constraintsButtonSubmit = new GridBagConstraints(
+                2, 5, 2, 1, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(1, 1, 1, 1), 0, 0);
+        mainPanel.add(buttonSubmit, constraintsButtonSubmit);
+
 
         windowGridBagLayout.setContentPane(mainPanel);
         windowGridBagLayout.revalidate();
