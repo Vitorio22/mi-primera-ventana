5bf1a1b1023a0a4e4bfb5a7f594fdf6ac659940e
Victor <nicromante656@hotmail.com>
2022-10-14 11:46:40 +0200

    Options Window con metodos

--- src/org/iesfm/ventana/MoveOptionsWindow.java
+++ src/org/iesfm/ventana/MoveOptionsWindow.java
@@ -5,6 +5,11 @@ import java.awt.*;
 
 public class MoveOptionsWindow extends JFrame {
 
+    private JPanel mainPanel;
+
+
+
+
     public static void main(String[] args) {
 
         JFrame f = new JFrame("Move Options");
@@ -15,11 +20,49 @@ public class MoveOptionsWindow extends JFrame {
         this.setVisible(true);
         this.setResizable(false);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setContentPane(createMainPanel());
+        repaint();
+        revalidate();
     }
 
-    public JPanel mainPanel() {
+    private JPanel createMainPanel() {
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,5));
+        mainPanel.add(createListWest());
         return mainPanel;
     }
+
+    private JList createListWest(){
+
+        return createListWest();
+    }
+
+    private JList createListEast(){
+
+        return createListEast();
+    }
+
+    private JButton toRight(){
+
+        return toRight();
+    }
+    private JButton toLeft(){
+
+        return toLeft();
+    }
+
+    private JButton addAll(){
+
+        return addAll();
+    }
+
+    private JButton deleteAll(){
+
+        return deleteAll();
+    }
+
+
+
+
 }
